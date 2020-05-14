48558ab00df6d64f462e681865b93a5402f37400
krishna Gupta <krishgupta43@gmail.com>
2020-05-15 02:28:11 +0530

    Updates

---
+++ precache-manifest.6fb10eadc84a99066fef625d749b51ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26db5ddb277306f1a3c2df081d6b307c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f33b9d746b2a9da190fd",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/my-app/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33b9d746b2a9da190fd",
+    "url": "/my-app/static/js/main.b253d1bb.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  }
+]);
